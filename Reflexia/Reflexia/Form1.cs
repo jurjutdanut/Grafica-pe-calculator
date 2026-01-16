57b29941f85760c342d2313613d867f2ac192996
jurjutdanut <146926315+jurjutdanut@users.noreply.github.com>
2026-01-17 00:16:56 +0200

    Add files via upload

---
+++ Reflexia/Reflexia/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reflexia
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Reflexia";
+            this.Size = new Size(800, 600);
+            this.BackColor = Color.White;
+            this.DoubleBuffered = true;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            PointF[] originalRect = new PointF[]
+            {
+                new PointF(100,100),
+                new PointF(200,100),
+                new PointF(200,200),
+                new PointF(100,200)
+            };
+            g.DrawPolygon(new Pen(Color.Blue, 2), originalRect);
+            float scale = 2.0f;
+            PointF[] scaledRect = new PointF[originalRect.Length];
+            for (int i = 0; i < originalRect.Length; i++)
+            {
+                scaledRect[i] = new PointF(originalRect[i].X * scale, originalRect[i].Y * scale);
+            }
+            g.DrawPolygon(new Pen(Color.Red, 2), scaledRect);
+            base.OnPaint(e);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Reflexia/Reflexia/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Reflexia")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Reflexia")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("aeed90c6-a4cc-4111-9f3a-dd31fb405576")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
