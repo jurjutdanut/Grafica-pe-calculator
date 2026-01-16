fdd447a031e383471ed0757dd1a5d536dc915aaf
jurjutdanut <146926315+jurjutdanut@users.noreply.github.com>
2026-01-17 00:16:14 +0200

    Add files via upload

---
+++ Lambert/Lambert/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lambert
+{
+    public partial class Form1 : Form
+    {
+        const int gridSize = 100;
+        float[,] heightMap = new float[gridSize, gridSize];
+
+        float lightX = 0.5f;
+        float lightY = 0.5f;
+        float lightZ = -1f;
+        float kd = 1.5f;
+        float IL = 1.5f;
+        public Form1()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+            this.ClientSize = new Size(600,600);
+            GenerateSurface();
+        }
+
+        void GenerateSurface()
+        {
+            for (int x = 0; x < gridSize; x++)
+            {
+                for (int y = 0; y < gridSize; y++)
+                {
+                    float dx = x - gridSize / 2f;
+                    float dy = y - gridSize / 2f;
+                    heightMap[x, y] = (float)(50 * Math.Sin(dx * 0.1f) * Math.Cos(dy * 0.1f));
+                }
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            DrawLambert(e.Graphics);
+        }
+
+        void DrawLambert(Graphics g)
+        {
+            int cell = Math.Min(ClientSize.Width, ClientSize.Height) / gridSize;
+
+            for (int x = 1; x < gridSize - 1; x++)
+            {
+                for (int y = 1; y < gridSize - 1; y++)
+                {
+                    float xL = x - 1, yL = y, zL = heightMap[x - 1, y];
+                    float xR = x + 1, yR = y, zR = heightMap[x + 1, y];
+                    float xD = x, yD = y - 1, zD = heightMap[x, y - 1];
+                    float xU = x, yU = y + 1, zU = heightMap[x, y + 1];
+
+                    float dxX = xR - xL;
+                    float dxY = yR - yL;
+                    float dxZ = zR - zL;
+
+                    float dyX = xU - xD;
+                    float dyY = yU - yD;
+                    float dyZ = zU - zD;
+
+                    float nX = dxY * dyZ - dxZ * dyY;
+                    float nY = dxZ * dyX - dxX * dyZ;
+                    float nZ = dxX * dyY - dxY * dyX;
+
+                    float length = (float)Math.Sqrt(nX * nX + nY * nY + nZ * nZ);
+                    if (length != 0)
+                    {
+                        nX /= length;
+                        nY /= length;
+                        nZ /= length;
+                    }
+
+                    float ndotl = Math.Max(0f, nX * lightX + nY * lightY + nZ * lightZ);
+                    float intensity = kd * IL * ndotl;
+
+                    int shade = (int)(intensity * 255);
+                    if (shade < 0) shade = 0;
+                    if (shade > 255) shade = 255;
+
+                    using (SolidBrush b = new SolidBrush(Color.FromArgb(shade, shade, shade)))
+                    {
+                        g.FillRectangle(b, x * cell, y * cell, cell, cell);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Lambert/Lambert/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Lambert")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lambert")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dfd82ba3-2271-415f-a90a-60409b8c1455")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
