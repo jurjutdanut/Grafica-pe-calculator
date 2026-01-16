8d4d7f40a72dc2bab77c92c5896e746566b3ab0c
jurjutdanut <146926315+jurjutdanut@users.noreply.github.com>
2026-01-17 00:11:15 +0200

    Add files via upload

---
+++ Cercul/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Cercul
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        void DrawCircleBresenham(Graphics g, int x0, int y0, int r, Pen pen)
+        {
+            int x = 0, y = r;
+            int d = 3 - 2 * r;
+
+            while (x <= y)
+            {
+                DrawSymmetricPoints(g, x0, y0, x, y, pen);
+                if (d < 0)
+                {
+                    d = d + 4 * x + 6;
+                }
+                else
+                {
+                    d = d + 4 * (x - y) + 10;
+                    y--;
+                }
+                x++;
+            }
+        }
+
+        void DrawSymmetricPoints(Graphics g, int x0, int y0, int x, int y, Pen pen)
+        {
+            g.DrawRectangle(pen, x0 + x, y0 + y, 1, 1);
+            g.DrawRectangle(pen, x0 - x, y0 + y, 1, 1);
+            g.DrawRectangle(pen, x0 + x, y0 - y, 1, 1);
+            g.DrawRectangle(pen, x0 - x, y0 - y, 1, 1);
+            g.DrawRectangle(pen, x0 + y, y0 + x, 1, 1);
+            g.DrawRectangle(pen, x0 - y, y0 + x, 1, 1);
+            g.DrawRectangle(pen, x0 + y, y0 - x, 1, 1);
+            g.DrawRectangle(pen, x0 - y, y0 - x, 1, 1);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen pen = new Pen(Color.Blue, 1);
+            DrawCircleBresenham(g, 200, 200, 100, pen);
+        }
+    }
+}
---
+++ Cercul/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Cercul")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Cercul")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6b594d12-da03-4d54-b1e3-07da4b53a818")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
