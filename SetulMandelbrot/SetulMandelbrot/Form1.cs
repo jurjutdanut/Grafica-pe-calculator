23ae4beda2b8a5c65b91625ac9839e49fde0d0b9
jurjutdanut <146926315+jurjutdanut@users.noreply.github.com>
2026-01-17 00:23:54 +0200

    Add files via upload

---
+++ SetulMandelbrot/SetulMandelbrot/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SetulMandelbrot
+{
+    public partial class Form1 : Form
+    {
+        const int LATIME = 800;
+        const int INALTIME = 600;
+        const int MAX_ITER = 500;
+
+        double minRe = -2.5;
+        double maxRe = 1.0;
+        double minIm = -1.0;
+        double maxIm = 1.0;
+
+        public Form1()
+        {
+            this.Text = "Setul Mandelbrot";
+            this.Size = new Size(LATIME, INALTIME);
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            Bitmap bmp = new Bitmap(LATIME, INALTIME);
+
+            for (int px = 0; px < LATIME; px++)
+            {
+                for(int py = 0; py < INALTIME; py++)
+                {
+                    double cr = minRe + px * (maxRe - minRe) / LATIME;
+                    double ci = maxIm - py * (maxIm - minIm) / INALTIME;
+
+                    double zr = 0;
+                    double zi = 0;
+                    int iter = 0;
+
+                    while(zr * zr + zi * zi <= 4 && iter < MAX_ITER)
+                    {
+                        double temp = zr * zr - zi * zi + cr;
+                        zi = 2 * zr * zi + ci;
+                        zr = temp;
+                        iter++;
+                    }
+
+                    bmp.SetPixel(px, py, iter == MAX_ITER ? Color.Black : Color.Blue);
+                }
+            }
+            pictureBox1.Image = bmp;
+        }
+    }
+}
---
+++ SetulMandelbrot/SetulMandelbrot/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SetulMandelbrot")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SetulMandelbrot")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4f9e7650-7e86-4478-a8dc-4302944b989f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
